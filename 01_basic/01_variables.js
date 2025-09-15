7eb77fce9acaa4b6e69c90e3b4e01642faddaaa4
Deepak Kansari <die.hard0069@gmail.com>
2025-09-15 07:31:29 +0000

    01 variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+// Investigation
+const accountId ="1445"
+let accountEmail = "Sampal.02@gmail.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+let accountState;
+//console.log(accountId);
+// console.log(accountState);
+// console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+//Prefer not to use 'var' since its old and not follow any block & functional Scopes.
+// const variables cannot be changed after created like some variable need not to change such as id & passwords etc
+
