ef189949ca4270a51a153420d433a070c32064c1
Deepak Kansari <die.hard0069@gmail.com>
2025-09-15 07:50:14 +0000

    dataTypes

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -1,10 +1,15 @@
 "use strict";// this will treat all JS code as newer version
 
-// alert(3+3) // I am using nodejs, not browser
-
-console.log(3+3)
-
-console.log("Deepak")
-
+// alert(3+3) // I am using nodejs, not browser 
 // code should be redable and east to understand
-// TC39,MDN - this are the documentation to study and understand codes
+// // TC39,MDN - this are the documentation to study and understand codes
+
+/*Types
+// Strings -"abcd",'abcd'
+// Numbers - 12445
+// boolean - true,false
+// bigint - large numbers mostly for large no of data (reddit,stock market apps etc)
+// null - Javascript look this as object.Its a Standalone value
+//  undefined - It has no vaulue but has variable or the given variable value has been not set
+//  Symbol - Define individual component (unique)
+//  object 
