deb96516a1a415ab3339ac834ec6eafed4b3419b
Deepak Kansari <die.hard0069@gmail.com>
2025-09-15 07:31:13 +0000

    02_datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict";// this will treat all JS code as newer version
+
+// alert(3+3) // I am using nodejs, not browser
+
+console.log(3+3)
+
+console.log("Deepak")
+
+// code should be redable and east to understand
+// TC39,MDN - this are the documentation to study and understand codes
